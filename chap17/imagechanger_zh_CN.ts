3ff1ee0e7e0673558ad85957ffc2210de48732b0
won2930015 <58769598@qq.com>
2018-02-09 01:10:45 +0800

    第18章::buildingservicesclient.pyw 网络应用

--- chap17/imagechanger_zh_CN.ts
+++ chap17/imagechanger_zh_CN.ts
@@ -27,342 +27,342 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="imagechanger.pyw" line="63"/>
+        <location filename="imagechanger.pyw" line="62"/>
         <source>Ready</source>
         <translation>读</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="65"/>
+        <location filename="imagechanger.pyw" line="64"/>
         <source>&amp;New...</source>
         <translation>新建(&amp;N)...</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="65"/>
+        <location filename="imagechanger.pyw" line="64"/>
         <source>Create an image file</source>
         <translation>创建图像文件</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="68"/>
+        <location filename="imagechanger.pyw" line="67"/>
         <source>&amp;Open...</source>
         <translation>打开(&amp;O)...</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="68"/>
+        <location filename="imagechanger.pyw" line="67"/>
         <source>Open an existing image file</source>
         <translation>打开存在的图像文件</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="71"/>
+        <location filename="imagechanger.pyw" line="70"/>
         <source>&amp;Save</source>
         <translation>保存(&amp;S)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="71"/>
+        <location filename="imagechanger.pyw" line="70"/>
         <source>Save the image</source>
         <translation>保存这图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="74"/>
+        <location filename="imagechanger.pyw" line="73"/>
         <source>Save &amp;As...</source>
         <translation>保存&amp;As...</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="74"/>
+        <location filename="imagechanger.pyw" line="73"/>
         <source>Save the image using a new name</source>
         <translation>保存图像到新的文件名</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="77"/>
+        <location filename="imagechanger.pyw" line="76"/>
         <source>&amp;Print</source>
         <translation>打印(&amp;P)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="77"/>
+        <location filename="imagechanger.pyw" line="76"/>
         <source>Print the image</source>
         <translation>打印图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="80"/>
+        <location filename="imagechanger.pyw" line="79"/>
         <source>&amp;Quit</source>
         <translation>退出(Q)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="80"/>
+        <location filename="imagechanger.pyw" line="79"/>
         <source>Ctrl+Q</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="80"/>
+        <location filename="imagechanger.pyw" line="79"/>
         <source>Close the application</source>
         <translation>结束这应用</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="83"/>
+        <location filename="imagechanger.pyw" line="82"/>
         <source>&amp;Invert</source>
         <translation>反相(&amp;I)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="83"/>
+        <location filename="imagechanger.pyw" line="82"/>
         <source>Ctrl+I</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="83"/>
+        <location filename="imagechanger.pyw" line="82"/>
         <source>Invert the image&apos;s colors</source>
         <translation>反相这图像颜色</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="87"/>
+        <location filename="imagechanger.pyw" line="86"/>
         <source>Sw&amp;ap Red and Blue</source>
         <translation>交换红蓝(&amp;A)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="87"/>
+        <location filename="imagechanger.pyw" line="86"/>
         <source>Ctrl+A</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="87"/>
+        <location filename="imagechanger.pyw" line="86"/>
         <source>Swap the image&apos;s red and blue color components</source>
         <translation>交换图像红与蓝颜色部分</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="92"/>
+        <location filename="imagechanger.pyw" line="91"/>
         <source>&amp;Zoom...</source>
         <translation>缩放(&amp;Z)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="92"/>
+        <location filename="imagechanger.pyw" line="91"/>
         <source>Alt+Z</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="150"/>
+        <location filename="imagechanger.pyw" line="149"/>
         <source>Zoom the image</source>
         <translation>缩放图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="95"/>
+        <location filename="imagechanger.pyw" line="94"/>
         <source>&amp;Resize...</source>
         <translation>调整大小(&amp;R)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="95"/>
+        <location filename="imagechanger.pyw" line="94"/>
         <source>Ctrl+R</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="95"/>
+        <location filename="imagechanger.pyw" line="94"/>
         <source>Resize the image</source>
         <translation>调整图像大小</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="99"/>
+        <location filename="imagechanger.pyw" line="98"/>
         <source>&amp;Unmirror</source>
         <translation>取消镜像(&amp;U)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="99"/>
+        <location filename="imagechanger.pyw" line="98"/>
         <source>Ctrl+U</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="99"/>
+        <location filename="imagechanger.pyw" line="98"/>
         <source>Unmirror the image</source>
         <translation>取消图像镜像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="103"/>
+        <location filename="imagechanger.pyw" line="102"/>
         <source>Mirror &amp;Horizontally</source>
         <translation>水平镜像(&amp;H)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="103"/>
+        <location filename="imagechanger.pyw" line="102"/>
         <source>Ctrl+H</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="103"/>
+        <location filename="imagechanger.pyw" line="102"/>
         <source>Horizontally mirror the image</source>
         <translation>水平镜像图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="110"/>
+        <location filename="imagechanger.pyw" line="109"/>
         <source>Mirror &amp;Vertically</source>
         <translation>垂直镜像(&amp;V)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="110"/>
+        <location filename="imagechanger.pyw" line="109"/>
         <source>Ctrl+V</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="110"/>
+        <location filename="imagechanger.pyw" line="109"/>
         <source>Vertically mirror the image</source>
         <translation type="unfinished">垂直镜像图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="117"/>
+        <location filename="imagechanger.pyw" line="116"/>
         <source>&amp;About Image Changer</source>
         <translation>关行图像变换器</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="135"/>
+        <location filename="imagechanger.pyw" line="134"/>
         <source>&amp;Help</source>
         <translation>帮助(&amp;H)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="122"/>
+        <location filename="imagechanger.pyw" line="121"/>
         <source>&amp;File</source>
         <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="128"/>
+        <location filename="imagechanger.pyw" line="127"/>
         <source>&amp;Edit</source>
         <translation>编辑(&amp;E)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="132"/>
+        <location filename="imagechanger.pyw" line="131"/>
         <source>&amp;Mirror</source>
         <translation>镜像(&amp;M)</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="172"/>
+        <location filename="imagechanger.pyw" line="171"/>
         <source>Image Changer</source>
         <translation>图像变换器</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="215"/>
+        <location filename="imagechanger.pyw" line="214"/>
         <source>Image Changer - Unsaved Changes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="215"/>
+        <location filename="imagechanger.pyw" line="214"/>
         <source>Save unsaved changes?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="238"/>
+        <location filename="imagechanger.pyw" line="237"/>
         <source>Image Changer - {}[*]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="241"/>
+        <location filename="imagechanger.pyw" line="240"/>
         <source>Image Changer - Unnamed[*]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="243"/>
+        <location filename="imagechanger.pyw" line="242"/>
         <source>Image Changer[*]</source>
         <translation>图像变换器[*]</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="284"/>
+        <location filename="imagechanger.pyw" line="283"/>
         <source>Created new image</source>
         <translation>创建新图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="294"/>
+        <location filename="imagechanger.pyw" line="293"/>
         <source>Image Changer - Choose Image</source>
         <translation>图像变换器-选择图像</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="359"/>
+        <location filename="imagechanger.pyw" line="358"/>
         <source>Image files ({})</source>
         <translation>图像文件({})</translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="314"/>
+        <location filename="imagechanger.pyw" line="313"/>
         <source>Failed to read {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="326"/>
+        <location filename="imagechanger.pyw" line="325"/>
         <source>Loaded {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="344"/>
+        <location filename="imagechanger.pyw" line="343"/>
         <source>Saved as {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="348"/>
+        <location filename="imagechanger.pyw" line="347"/>
         <source>Failed to save {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="359"/>
+        <location filename="imagechanger.pyw" line="358"/>
         <source>Image Changer - Save Image</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="394"/>
+        <location filename="imagechanger.pyw" line="393"/>
         <source>Inverted</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="394"/>
+        <location filename="imagechanger.pyw" line="393"/>
         <source>Uninverted</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="404"/>
+        <location filename="imagechanger.pyw" line="403"/>
         <source>Swapped Red and Blue</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="404"/>
+        <location filename="imagechanger.pyw" line="403"/>
         <source>Unswapped Red and Blue</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="424"/>
+        <location filename="imagechanger.pyw" line="423"/>
         <source>Mirrored Horizontally</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="424"/>
+        <location filename="imagechanger.pyw" line="423"/>
         <source>Unmirrored Horizontally</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="435"/>
+        <location filename="imagechanger.pyw" line="434"/>
         <source>Mirrored Vertically</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="435"/>
+        <location filename="imagechanger.pyw" line="434"/>
         <source>Unmirrored Vertically</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="442"/>
+        <location filename="imagechanger.pyw" line="441"/>
         <source>Image Changer - Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="442"/>
+        <location filename="imagechanger.pyw" line="441"/>
         <source>Percent:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="458"/>
+        <location filename="imagechanger.pyw" line="457"/>
         <source>Resized to the same size</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="467"/>
+        <location filename="imagechanger.pyw" line="466"/>
         <source>Resized to {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="483"/>
+        <location filename="imagechanger.pyw" line="482"/>
         <source>About Image Changer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="imagechanger.pyw" line="483"/>
+        <location filename="imagechanger.pyw" line="482"/>
         <source>&lt;b&gt;Image Changer&lt;/b&gt; v {0}
                 &lt;p&gt;Copyright &amp;copy; 2007-10 Qtrac Ltd. 
                 All rights reserved.
@@ -375,37 +375,37 @@
 <context>
     <name>NewImageDlg</name>
     <message>
-        <location filename="newimagedlg.ui" line="13"/>
+        <location filename="ui_newimagedlg.py" line="95"/>
         <source>Image Chooser - New Image</source>
         <translation>图像选择-新图像</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="64"/>
+        <location filename="ui_newimagedlg.py" line="96"/>
         <source>Color</source>
         <translation>颜色</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="71"/>
+        <location filename="ui_newimagedlg.py" line="97"/>
         <source>&amp;Color...</source>
         <translation>颜色(&amp;C)...</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="81"/>
+        <location filename="ui_newimagedlg.py" line="98"/>
         <source>&amp;Brush pattern:</source>
         <translation>画笔样式(&amp;B)</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="91"/>
+        <location filename="ui_newimagedlg.py" line="99"/>
         <source>&amp;Width:</source>
         <translation>宽(&amp;W):</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="101"/>
+        <location filename="ui_newimagedlg.py" line="100"/>
         <source>&amp;Height:</source>
         <translation>高(&amp;H)...</translation>
     </message>
     <message>
-        <location filename="newimagedlg.ui" line="136"/>
+        <location filename="ui_newimagedlg.py" line="102"/>
         <source> px</source>
         <translation type="unfinished"></translation>
     </message>
@@ -431,7 +431,7 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="imagechanger.pyw" line="526"/>
+        <location filename="imagechanger.pyw" line="525"/>
         <source>Image Changer</source>
         <translation>图像变换器</translation>
     </message>
--- chap18/buildingservicesclient.pyw
+++ chap18/buildingservicesclient.pyw
@@ -12,20 +12,20 @@
 import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from PyQt4.QtNetwork import *
+from PyQt4.QtNetwork import *   #包含QTcpSocket(),QUdpSocket()等网络模块.
 
 MAC = "qt_mac_set_native_menubar" in dir()
 
-PORT = 9407
-SIZEOF_UINT16 = 2
+PORT = 9407     #端口号
+SIZEOF_UINT16 = 2   #2表示两字节
 
 
-class BuildingServicesClient(QWidget):  #建立_服务_客户端
+class BuildingServicesClient(QWidget):  #构建_服务_客户端
 
     def __init__(self, parent=None):
         super(BuildingServicesClient, self).__init__(parent)
 
-        self.socket = QTcpSocket()
+        self.socket = QTcpSocket()  #TcpSocket::Tcp套接字
         self.nextBlockSize = 0  #下一_块_尺寸
         self.request = None #请求
 
@@ -35,19 +35,21 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
         regex = QRegExp(r"[0-9](?:0[1-9]|[12][0-9]|3[0-4])")
         self.roomEdit.setValidator(QRegExpValidator(regex, self))   #setValidator::设置_验证器
         self.roomEdit.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
+
         dateLabel = QLabel("&Date")
         self.dateEdit = QDateEdit() #创建日期编缉控件.
         dateLabel.setBuddy(self.dateEdit)
         self.dateEdit.setAlignment(Qt.AlignRight|Qt.AlignVCenter)
-        self.dateEdit.setDate(QDate.currentDate().addDays(1))
+        self.dateEdit.setDate(QDate.currentDate().addDays(1))   #设置日期是当前日期+1天.
         self.dateEdit.setDisplayFormat("yyyy-MM-dd")    #setDisplayFormat::设置_显示_格式
-        responseLabel = QLabel("Response")
-        self.responseLabel = QLabel()
-        self.responseLabel.setFrameStyle(QFrame.StyledPanel|QFrame.Sunken)
 
-        self.bookButton = QPushButton("&Book")
+        responseLabel = QLabel("Response")  #Response::响应
+        self.responseLabel = QLabel()
+        self.responseLabel.setFrameStyle(QFrame.StyledPanel|QFrame.Sunken)  #setFrameStyle::设置_框架_样式, StyledPanel::可变面板, Sunken::凹陷
+
+        self.bookButton = QPushButton("&Book")  #Book::预订[这里解'预订',别一解是 '书']
         self.bookButton.setEnabled(False)
-        self.unBookButton = QPushButton("&Unbook")
+        self.unBookButton = QPushButton("&Unbook")  #Unbook::取消预订
         self.unBookButton.setEnabled(False)
         quitButton = QPushButton("&Quit")
         if not MAC:
@@ -57,7 +59,7 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
         buttonLayout = QHBoxLayout()
         buttonLayout.addWidget(self.bookButton)
         buttonLayout.addWidget(self.unBookButton)
-        buttonLayout.addStretch()
+        buttonLayout.addStretch()   #addStretch::加入_拉伸 控件
         buttonLayout.addWidget(quitButton)
         layout = QGridLayout()
         layout.addWidget(roomLabel, 0, 0)
@@ -69,16 +71,16 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
         layout.addLayout(buttonLayout, 2, 1, 1, 4)
         self.setLayout(layout)
 
-        self.connect(self.socket, SIGNAL("connected()"), self.sendRequest)
-        self.connect(self.socket, SIGNAL("readyRead()"), self.readResponse)
-        self.connect(self.socket, SIGNAL("disconnected()"),
-                     self.serverHasStopped)
+        self.connect(self.socket, SIGNAL("connected()"), self.sendRequest)  #connected::连接
+        self.connect(self.socket, SIGNAL("readyRead()"), self.readResponse) #readyRead::准备_读
+        self.connect(self.socket, SIGNAL("disconnected()"),     #disconnected::断开
+                     self.serverHasStopped) #serverHasStopped::服务_在_停止
         self.connect(self.socket,
                      SIGNAL("error(QAbstractSocket::SocketError)"),
-                     self.serverHasError)
-        self.connect(self.roomEdit, SIGNAL("textEdited(QString)"),
+                     self.serverHasError)   #serverHasError::服务_在_错误
+        self.connect(self.roomEdit, SIGNAL("textEdited(QString)"),  #textEdited::文本_编缉时
                      self.updateUi)
-        self.connect(self.dateEdit, SIGNAL("dateChanged(QDate)"),
+        self.connect(self.dateEdit, SIGNAL("dateChanged(QDate)"),   #dateChanged::时间_改变时
                      self.updateUi)
         self.connect(self.bookButton, SIGNAL("clicked()"), self.book)
         self.connect(self.unBookButton, SIGNAL("clicked()"), self.unBook)
@@ -87,24 +89,24 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
         self.setWindowTitle("Building Services")
 
 
-    def updateUi(self):
-        enabled = False
+    def updateUi(self): #更新Ui
+        enabled = False #enabled::启用(ed::时)
         if (self.roomEdit.text() and
             self.dateEdit.date() > QDate.currentDate()):
             enabled = True
-        if self.request is not None:
+        if self.request is not None:    #request::请求
             enabled = False
         self.bookButton.setEnabled(enabled)
         self.unBookButton.setEnabled(enabled)
 
 
     def closeEvent(self, event):
-        self.socket.close()
+        self.socket.close() #套接字.关闭
         event.accept()
 
 
     def book(self):
-        self.issueRequest("BOOK", self.roomEdit.text(),
+        self.issueRequest("BOOK", self.roomEdit.text(), #issueRequest::发出_请求
                           self.dateEdit.date())
 
 
@@ -113,27 +115,28 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
                           self.dateEdit.date())
 
 
-    def issueRequest(self, action, room, date):
-        self.request = QByteArray()
+    def issueRequest(self, action, room, date): #issueRequest::发出_请求, action::动作, room::房号, date::日期
+        self.request = QByteArray() #request::请求, QByteArray()::字节_数组[PS:保存在内存的数据格式,能像其他IODevice一样进行读写操作]
         stream = QDataStream(self.request, QIODevice.WriteOnly)
         stream.setVersion(QDataStream.Qt_4_2)
-        stream.writeUInt16(0)
-        stream.writeQString(action)
-        stream.writeQString(room)
-        stream << date
-        stream.device().seek(0)
-        stream.writeUInt16(self.request.size() - SIZEOF_UINT16)
+        stream.writeUInt16(0)   #P397② 写入'两字节无符号整数'.
+        stream.writeQString(action) #写入 动作
+        stream.writeQString(room)   #写入 房号
+        stream << date      #加入 日期
+        stream.device().seek(0) #P398③
+        stream.writeUInt16(self.request.size() - SIZEOF_UINT16) #size()::返回值以字节为单位.P398④
         self.updateUi()
-        if self.socket.isOpen():
+        if self.socket.isOpen():    #isOpen()::套接字是否打开.P398⑤
             self.socket.close()
         self.responseLabel.setText("Connecting to server...")
-        self.socket.connectToHost("localhost", PORT)
+        self.socket.connectToHost("localhost", PORT)    #执行connectToHost::连接_到_主机/宿主
+                                                        #触发socket::connected信号ROW:74↑,执行sendRequest()方法↓
 
 
-    def sendRequest(self):
+    def sendRequest(self):      #当该方法完成时会执行serverHasStopped()/serverHasError()
         self.responseLabel.setText("Sending request...")
         self.nextBlockSize = 0
-        self.socket.write(self.request)
+        self.socket.write(self.request) #向 套接字 写入 请求.
         self.request = None
         
 
@@ -143,7 +146,7 @@ class BuildingServicesClient(QWidget):  #建立_服务_客户端
 
         while True:
             if self.nextBlockSize == 0:
-                if self.socket.bytesAvailable() < SIZEOF_UINT16:
+                if self.socket.bytesAvailable() < SIZEOF_UINT16:    #bytesAvailable::可用_字节
                     break
                 self.nextBlockSize = stream.readUInt16()
             if self.socket.bytesAvailable() < self.nextBlockSize:
